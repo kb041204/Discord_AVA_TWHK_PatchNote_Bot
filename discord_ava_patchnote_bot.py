70e3b5b2a015693af0bc0454fda5c357e022c068
kb041204 <kb041204@hotmail.com>
2020-07-23 23:10:01 +0800

    Discord integration

---
+++ discord_ava_patchnote_bot.py
@@ -0,0 +1,91 @@
+import discord
+import asyncio
+
+import os
+from dotenv import load_dotenv
+
+import time
+import requests
+from bs4 import BeautifulSoup
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL = os.getenv('DISCORD_CHANNEL')
+CHECK_INTERVAL = os.getenv('AVA_CHECK_INTERVAL_IN_MINUTES')
+
+ava_url = "https://ava.mangot5.com"
+latest_notice_title = "none"
+hello_world = False
+check_interval = int(CHECK_INTERVAL) * 60
+
+client = discord.Client()
+curr_guild = None
+channel = None
+
+
+@client.event
+async def on_ready():
+	global hello_world, curr_guild, channel
+
+	curr_guild = discord.utils.get(client.guilds, name=GUILD)
+	if curr_guild is None:
+		print("Guild \"" + GUILD + "\" not found")
+		return
+		
+	channel = discord.utils.get(curr_guild.text_channels, name=CHANNEL)
+	if channel is None:
+		print("In guild: \"" + GUILD +  "\", text channel \"" + CHANNEL + "\" not found")
+		return
+		
+	msg = "This guild will now receive AVA news"
+	if hello_world == False:
+		#await channel.send(msg)
+		hello_world = True
+		print("Bot is ready")
+
+
+@client.event
+async def checking():
+	global ava_url, latest_notice_title, check_interval
+	
+	while(True):
+		res = requests.get(ava_url + "/game/ava/notice")
+		soup = BeautifulSoup(res.text, 'html.parser')
+
+		try:
+			list_of_notices = soup.find("tbody").find_all("a")
+
+			#The latest notice is changed?
+			if latest_notice_title != list_of_notices[0].text.strip():
+
+				#Extracting url of notice (notice_url)
+				notice_url = ava_url + list_of_notices[0]["href"]
+		
+				#Extracting title (latest_notice_title)
+				latest_notice_title = list_of_notices[0].text.strip()
+		
+				#Extracting content of notice (Date: notice_post_date, Content: notice_content)
+				notice_res = requests.get(notice_url)
+				notice_soup = BeautifulSoup(notice_res.text, 'html.parser')
+				notice_content = notice_soup.find("div", {"class": "view_contents"}).text.strip()
+				notice_post_date = notice_soup.find("dl", {"class": "fR"}).dd.text.strip()
+			
+				curr_guild = discord.utils.get(client.guilds, name=GUILD)
+				channel = discord.utils.get(curr_guild.text_channels, name=CHANNEL)
+				message = "Title: " + latest_notice_title + "\n\nTime: " + notice_post_date + "\n\nURL: " + str(notice_url) + "\n\n\n" + str(notice_content)
+				await channel.send(message)
+			
+				print("[Log] " + time.asctime(time.localtime(time.time())) +  ": Posted \"" + latest_notice_title + "\"")
+			
+			else:
+				print("[Log] " + time.asctime(time.localtime(time.time())) +  ": No update, latest notice title: " + latest_notice_title)
+				await asyncio.sleep(check_interval)
+		
+		except AttributeError:
+			retry_time = 10
+			print("[Error] " + time.asctime(time.localtime(time.time())) +  ": Error in AVA website server, re-try in " + str(retry_time) + " seconds...")
+			await asyncio.sleep(retry_time)
+
+client.loop.create_task(checking())
+client.run(TOKEN)
