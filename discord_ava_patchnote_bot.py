b05357a0e3091b71bc6f1fc7c4af96688463d0b5
kb041204 <kb041204@hotmail.com>
2020-07-27 01:11:06 +0800

    Fix error in latest log in log entry website

--- discord_ava_patchnote_bot.py
+++ discord_ava_patchnote_bot.py
@@ -86,13 +86,13 @@ async def checking():
 					channel = discord.utils.get(curr_guild.text_channels, name=CHANNEL)
 					message = str(latest_notice_title) + "\n\n" + str(notice_url) + "\n\n" + str(notice_content)
 					await channel.send(message)
-					write_to_log("[Log] " + time.asctime(time.localtime(time.time())) +  ": Posted in discord: \"" + latest_notice_title + "\"")
+					write_to_log("[Log] " + time.asctime(time.localtime(time.time())) +  ": Posted in discord: '" + latest_notice_title + "'")
 					log_count = 0
 					await asyncio.sleep(CHECK_INTERVAL_IN_SEC)
 				
 			else: #No update
 				if log_count == 0:
-					write_to_log("[Log] " + time.asctime(time.localtime(time.time())) +  ": No update, latest: \"" + latest_notice_title + "\"")
+					write_to_log("[Log] " + time.asctime(time.localtime(time.time())) +  ": No update, latest: '" + latest_notice_title + "'")
 				log_count = log_count + 1
 				if log_count == NO_UPDATE_LOG_OCCURANCE:
 					log_count = 0
